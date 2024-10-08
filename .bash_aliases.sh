7b3d9cd890c2de3cc69a913cf07308c6bbebc563
Julien-Fischer <julien.fischer@agiledeveloper.net>
2024-10-08 19:56:46 +0200

    feat: Print current battery capacity

--- .bash_aliases.sh
+++ .bash_aliases.sh
@@ -204,6 +204,10 @@ cpu_usage() {
     grep 'cpu' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}'
 }
 
+bat_capacity() {
+    cat /sys/class/power_supply/BAT0/capacity
+}
+
 ##############################################################
 # Network
 ##############################################################
