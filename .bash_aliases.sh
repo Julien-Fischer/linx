124dbca46636b74ab3d27203e15ed3623ca4b251
Julien-Fischer <julien.fischer@agiledeveloper.net>
2024-10-10 18:47:42 +0200

    feat: Add hardware function

--- .bash_aliases.sh
+++ .bash_aliases.sh
@@ -217,6 +217,7 @@ alias byebye='systemctl poweroff'
 alias reboot='systemctl reboot'
 alias mem='free -m -l -t'
 alias du='du -h --max-depth=1'
+alias hardware='neofetch' # or any other tool or custom function that fits your needs
 
 drives() {
     mount | awk -F' ' '{printf \"%s\t%s\n\",\$1,\$3; }' | column -t | grep -E ^/dev/ | sort
