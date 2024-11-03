d89b086345d7d4c0463a05fb70c3e054c9e9333e
Julien-Fischer <julien.fischer@agiledeveloper.net>
2024-11-03 06:33:30 +0100

    test: Test timestamp function

--- tests/suites/core-tests.sh
+++ tests/suites/core-tests.sh
@@ -5,8 +5,14 @@
 # https://github.com/Julien-Fischer/linx/blob/master/LICENSE
 
 export CORE_TESTS=(
-    "is_installed"
-    "lt_lists_dir_content"
+    'is_installed'
+    'lt_lists_dir_content'
+    'it_uses_default_timestamp'
+    'it_uses_custom_date_separator'
+    'it_uses_custom_date_time_separator'
+    'it_uses_custom_time_separator'
+    'it_uses_all_custom_separators'
+    'it_uses_timestamp_format'
 )
 
 # A smoke test asserting that the Test environment is set up
@@ -21,4 +27,39 @@ lt_lists_dir_content() {
     if ! lt > /dev/null; then
         return 1
     fi
+}
+
+it_uses_default_timestamp() {
+    local result=$(timestamp)
+    local expected=$(date "+%Y-%m-%d %H:%M:%S")
+    assert_equals "$expected" "$result"
+}
+
+it_uses_custom_date_separator() {
+    local result=$(timestamp "/")
+    local expected=$(date "+%Y/%m/%d %H:%M:%S")
+    assert_equals "$expected" "$result"
+}
+
+it_uses_custom_date_time_separator() {
+    local result=$(timestamp "-" "_")
+    local expected=$(date "+%Y-%m-%d_%H:%M:%S")
+    assert_equals "$expected" "$result"
+}
+
+it_uses_custom_time_separator() {
+    local result=$(timestamp "-" " " ",")
+    local expected=$(date "+%Y-%m-%d %H,%M,%S")
+    assert_equals "$expected" "$result"
+}
+
+it_uses_all_custom_separators() {
+    local result=$(timestamp "/" "_" ",")
+    local expected=$(date "+%Y/%m/%d_%H,%M,%S")
+    assert_equals "$expected" "$result"
+}
+
+it_uses_timestamp_format() {
+    local result=$(timestamp)
+    assert_matches "$result" "^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$"
 }
