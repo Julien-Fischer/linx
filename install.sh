c9746463705e4a1d1e15b7a320219b1a1a1db59d
Julien-Fischer <julien.fischer@agiledeveloper.net>
2024-11-05 00:26:55 +0100

    chore: Increment version number

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 # Constants
 ##############################################################
 
-export VERSION="1.0.0-alpha2"
+export VERSION="1.0.0-alpha3"
 export PROJECT="linx"
 export LINX_DIR="${HOME}/${PROJECT}"
 INSTALL_DIR="tmp-linx-install"
