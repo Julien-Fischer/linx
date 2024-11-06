7dd9ad8b497041182597c78ac51336e9ab9c02eb
Julien-Fischer <julien.fischer@agiledeveloper.net>
2024-11-06 23:22:06 +0100

    feat: Add -c, --commands parameter to linx command

--- commands/linx.sh
+++ commands/linx.sh
@@ -11,32 +11,61 @@ else
 fi
 source "${HOME}"/.bashrc
 
+# doc
+
+USAGE=$(cat <<EOF
+Usage: linx [OPTIONS]
+
+Backup the element identified by the specified path. If the element to backup is
+a directory, copy it recursively.
+
+Arguments:
+  sync                    Synchronize the local setup with the remote
+
+Options:
+  -c, --commands          List the native Linx commands
+  -h, --help              Show this message and exit
+  -v, --version           Show Linx version and exit
+EOF
+)
+
+pull_setup() {
+    if install_core; then
+        echo "${PROJECT} successfully synced"
+    else
+        echo "${PROJECT}: failed to sync"
+        return 1
+    fi
+}
+
 # @description Synchronize the local linx installation with the latest version from the remote
 # @return 0 if the configuration was synchronized successfully; 1 otherwise
 linx() {
     while [[ $# -gt 0 ]]; do
         case $1 in
             sync)
-                if install_core; then
-                    echo "${PROJECT} successfully synced"
-                else
-                    echo "${PROJECT}: failed to sync"
-                    return 1
-                fi
-                shift
+                pull_setup
                 return 0
                 ;;
             -v|--version)
                 echo "${VERSION}"
                 return 0
                 ;;
+            -h|--help)
+                echo "${USAGE}"
+                return 0
+                ;;
+            -c|--commands)
+                cat "${LINX_INSTALLED_COMMANDS}"
+                return 0
+                ;;
             *)
-                shift
+                err "Invalid parameter ${1}"
+                echo "${USAGE}"
+                return 1
                 ;;
         esac
     done
-    echo "Usage: linx [sync|--version]"
-    return 1
 }
 
 linx "$@"
